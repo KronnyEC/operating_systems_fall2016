92e921b3b9abcfac5b5d1460a4b4673c80c6c59d
Manny Meinen <manny@bastion.meinen>
2016-09-22 11:25:24 -0500

    First commit - adding base template for fsend class

---
+++ fsend.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int server (int port) {
+  //open listen socket on port
+
+  //wait for input
+  
+  //close socket
+}
+
+int client (char* addr, int port) {
+  //open socket and connect to addr on port
+  
+  //send data over socket
+
+  //close socket
+}
+
+int main () {
+  printf("foo");
+  return 0;
+}
