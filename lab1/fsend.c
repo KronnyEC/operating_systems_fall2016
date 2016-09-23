65c110e9b6d8faab73ee5451ccc8fd53bc52f736
Erik Kronholm <erikkronholm@Eriks-MacBook-Air.local>
2016-09-22 21:13:20 -0500

    added server setup, and argv input parsing

--- lab1/fsend.c
+++ lab1/fsend.c
@@ -1,8 +1,55 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include "sockets.h"
 
-int server (int port) {
+
+int server (int portno) {
+  //Get a soocket of the right type for now test with int sockfd
+	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+	if (sockfd < 0){
+		printf("Error opening socket");
+		exit(1);
+	}
+
+	printf("Server Socket started on Port: %d\n", portno);
+  // port number
+
+
+  // server address structure
+  struct sockaddr_in serv_addr;
+
+  // Set all the values in the server address to 0
+  memset(&serv_addr, '0', sizeof(serv_addr));
+
+  // Setup the type of socket (internet vs filesystem)
+  serv_addr.sin_family = AF_INET;
+
+  // Basically the machine we are on...
+  serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+  // Setup the port number
+  // htons - is host to network byte order
+  // network byte order is most sig bype first
+  //   which might be host or might not be
+  serv_addr.sin_port = htons(portno);
+
+  // Bind the socket to the given port
+  if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+    printf("ERROR on binding\n");
+    exit(1);
+  }
+
+  // set it up to listen
+  listen(sockfd,5);
+
+  return sockfd;
   //open listen socket on port
-  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
   //wait for input
   
   //close socket
@@ -16,20 +63,32 @@ int client (char* addr, int port) {
   //close socket
 }
 
-int main (int argc, char *argv[]) {
+void parse(char *line, char **argv){
+	while (*line != '\0') {       /* if not the end of line ....... */
+          	while (*line == ' ' || *line == '\t' || *line == '\n')
+               		*line++ = '\0';     /* replace white spaces with 0    */
+          		*argv++ = line;          /* save the argument position     */
+          	while (*line != '\0' && *line != ' ' &&
+                 	*line != '\t' && *line != '\n')
+               		line++;             /* skip the argument until ...    */
+     }
+     *argv = '\0';                 /* mark the end of argument list  */
+}
 
-  //parse input
-  //IP, Port, etc.
+int main(){ 
+  char line [1024]; //this might need to change for input
+  char *argv[64];
 
-  //are we a server or a client?
-  char *listen = "-l";
-  if (argc > 1 && *argv[1] == *listen) {
-    printf("server\n");
-    //server(9285)
-  } else {
-    printf("client\n");
-    //client(localhost, 9285)
-  }
-
-  return 0;
+	
+	while (1) {                   /* repeat until done ....         */
+          printf("Shell -> ");     /*   display a prompt             */
+          gets(line);              /*   read in the command line     */
+          printf("\n");
+          parse(line, argv);       /*   parse the line               */
+          //if (strcmp(argv[0], "exit") == 0)  /* is it an "exit"?     */
+               //exit(0);            /*   exit if it is                */
+          //execute(argv);           /* otherwise, execute the command */
+     }
+	
+	
 }
